a1b367cbb4d8abb05cddb6369176f1f9bfc746bf
Kenn <83052195+Denny123141@users.noreply.github.com>
2021-12-07 12:36:59 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,17 +1,6 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI BISA VIA E-MONEY
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
@@ -21,7 +10,7 @@ return `DONASI BISA VIA BANK / E-MONEY
 3. DANA
 	ㅁ 0887 4350 47326
 	
-Semuanya atas nama RI****DI
+DONASI SE-IKLASNYA (TIDAK ADA UNSUR PAKSA)
 `
 	}
 
